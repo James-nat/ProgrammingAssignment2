7f0bf315cde07434588159c2f2d918a1fc5e0c99
James-nat <63943035+James-nat@users.noreply.github.com>
2020-04-19 18:29:47 -0400

    Update cachematrix.R
    Updated file with the two assigned functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix creates a matrix-like object which caches it's own inverse.
+## When cacheSolve takes a result from MakeCacheMatrix as an argument, 
+## cacheSolve checks to see if the inverse has been calculated and returns the cached inverse instead of recalculating it. 
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+## set the value of the matrix
+  set <- function(y) {                      
+    x <<- y
+    m <<- NULL
+  }                                        
+  get <- function() x                       ## get the value of the matrix
+  setsolve <- function(solve) m <<- solve   ## set the value of the inverse
+  getsolve <- function() m                  ## get the value of the inverse
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
 
+## This function computes the inverse of the special matrix returned by makeCacheMatrix above.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  m <- x$getsolve()
+  if(!is.null(m)) {			## If the inverse has already been calculated (and the matrix has not changed) 
+    message("getting cached data")	## it retrieve the inverse from the cache and skips the computation.
+    return(m)
+  }
+  data <- x$get()				                                                  
+  m <- solve(data, ...)		        ## the solve function calculates the inverse of the special matix returned by makeCacheMatrix
+  x$setsolve(m)                         ## the setsolve function sets the inverse in the cache
+  m
 }
